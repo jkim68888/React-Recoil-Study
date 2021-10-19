dfebdb4af77d1e7e4a2b5c447c9d0e20f92be23c
jkim68888 <jkim68888@gmail.com>
2021-10-19 13:47:00 +0900

    <Grandchild />: recoil을 통해 전역 상태 text를 가져와 <input type="text" />로 보여준다!

--- src/Grandchild.js
+++ src/Grandchild.js
@@ -1,7 +1,21 @@
+import { useRecoilState } from "recoil";
+import textState from "./atoms";
+
 function Grandchild() {
+  const [text, setText] = useRecoilState(textState);
+
   return (
     <div>
       <h3>Grandchild.js</h3>
+      <p>
+        <input
+          type="text"
+          value={text}
+          readOnly
+          // readOnly란?
+          // 참고: https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/readonly
+        />
+      </p>
     </div>
   );
 }
