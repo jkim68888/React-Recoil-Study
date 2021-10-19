dcca3371adbb5c263d64bc97ceb6193faba8332b
jkim68888 <jkim68888@gmail.com>
2021-10-19 13:26:16 +0900

    <RecoilRoot />를 진짜 최상단인 index.js로 옮김

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {
-  RecoilRoot,
+  // RecoilRoot,
   atom,
   selector,
   useRecoilState,
@@ -11,23 +11,22 @@ import "./App.css";
 function App() {
   return (
     <RecoilRoot>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </RecoilRoot>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
